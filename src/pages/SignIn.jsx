514d1c2ed2bf8628f3104192f91933dbc9889377
Galina-S <galyna.sazonova@gmail.com>
2023-06-21 13:47:32 +0200

    Authorization with Google

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import {AiFillEye, AiFillEyeInvisible} from 'react-icons/ai';
 import OAuth from '../components/OAuth';
 
-
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({email: '', password: ''});
